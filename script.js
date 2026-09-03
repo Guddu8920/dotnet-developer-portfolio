385fe05c5d9b97d6449ba305ff1632b96a135e6e
Guddu8920 <131642597+Guddu8920@users.noreply.github.com>
2026-09-03 13:40:38 +0530

    Delete script.js

--- script.js
+++
@@ -1,1 +0,0 @@
-const menuBtn=document.getElementById("menuBtn"),navLinks=document.getElementById("navLinks");menuBtn.addEventListener("click",()=>navLinks.classList.toggle("open"));document.querySelectorAll(".nav-links a").forEach(a=>a.addEventListener("click",()=>navLinks.classList.remove("open")));document.getElementById("year").textContent=new Date().getFullYear();
